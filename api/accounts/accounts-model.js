80cf132e75a95f988c55a0eac0120884c4f674cf
ctscofield <ctscofield@gmail.com>
2021-07-12 21:04:13 -0600

    post endpoint

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -5,12 +5,12 @@ const getAll = () => {
 }
 
 const getById = id => {
-  // DO YOUR MAGIC
   return db('accounts').where('id', id).first()
 }
 
-const create = account => {
-  // DO YOUR MAGIC
+const create = async account => {
+  const [id] = await db('accounts').insert(account)
+  return getById(id)
 }
 
 const updateById = (id, account) => {
